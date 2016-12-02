d3d9288f3ff6c4b913fdc2079a2caed2b25b19d0
nooralim <muhib.noorali2@gmail.com>
2016-12-02 11:51:43 -0800

    pushed with work in progress SearchBar

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -7,12 +7,16 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.List;
 import java.util.UUID;
 
-public class CrimePagerActivity extends FragmentActivity {
+public class CrimePagerActivity extends FragmentActivity implements SearchView.OnQueryTextListener {
 
     private static final String EXTRA_CRIME_ID =
             "com.bignerdranch.android.criminalintent.crime_id";
@@ -58,5 +62,30 @@ public class CrimePagerActivity extends FragmentActivity {
                 break;
             }
         }
+    }//onCreate end
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.searchbartest, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.searcher);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(this);
+
+        return true;
+
+
+    }//onCCreateoptions menu end
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        //User pressed search
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        //user changed text
+        return false;
     }
 }
